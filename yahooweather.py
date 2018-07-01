2686fc868bcfb8323983bed17bcb36e257bcf9ea
Ashton Jordan <ashton.jordan@gmail.com>
2018-06-30 22:26:56 -0500

    Changed Unit from C to F, and updated the Low/High Temp variable, and modifed the while loop variable

--- yahooweather.py
+++ yahooweather.py
@@ -1,20 +1,21 @@
 from weather import Weather, Unit
 
-weather = Weather(unit=Unit.CELSIUS)
+weather = Weather(unit=Unit.FAHRENHEIT)
 
 print('Welcome to the ACME Weather Program' + '\n')
 
 def forecast(x,y):
+#Yahoo Weather App API Documentation: https://developer.yahoo.com/weather/documentation.html?guccounter=1
     location = weather.lookup_by_location(x)
     forecasts = location.forecast
     for x in range(0,y):
         print ('\n' + 'Date: ' + forecasts[x].day+ ' ' + forecasts[x].date)
         print ('Weather Conditions: '+ forecasts[x].text)
-        print ('Hight Temp: ' + forecasts[x].date)
-        print ('Low Temp: ' + forecasts[x].date + '\n')
+        print ('Hight Temp: ' + forecasts[x].high)
+        print ('Low Temp: ' + forecasts[x].low + '\n')
 
-x = True
-while x != False:
+skyisblue = True
+while skyisblue != False:
     city = input("Enter your city: ")
     number_of_days = input("Number of day you would like the forecast? (1 - 10) ")
 
