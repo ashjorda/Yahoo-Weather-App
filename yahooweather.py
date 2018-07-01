216c0e54beb9196933ff696ff5bb8125f9cdbbe4
Ashton Jordan <ashton.jordan@gmail.com>
2018-07-01 00:14:15 -0500

    Updated forcast function with corresponding input variable names, modified the input message for "city"

--- yahooweather.py
+++ yahooweather.py
@@ -4,11 +4,11 @@ weather = Weather(unit=Unit.FAHRENHEIT)
 
 print('Welcome to the ACME Weather Program' + '\n')
 
-def forecast(x,y):
+def forecast(city,number_of_days):
 #Yahoo Weather App API Documentation: https://developer.yahoo.com/weather/documentation.html?guccounter=1
-    location = weather.lookup_by_location(x)
+    location = weather.lookup_by_location(city)
     forecasts = location.forecast
-    for x in range(0,y):
+    for x in range(0,number_of_days):
         print ('\n' + 'Date: ' + forecasts[x].day+ ' ' + forecasts[x].date)
         print ('Weather Conditions: '+ forecasts[x].text)
         print ('Hight Temp: ' + forecasts[x].high)
@@ -16,8 +16,8 @@ def forecast(x,y):
 
 skyisblue = True
 while skyisblue != False:
-    city = input("Enter your city: ")
-    number_of_days = input("Number of day you would like the forecast? (1 - 10) ")
+    city = input("Enter the city you would like the weather for: ")
+    number_of_days = input("Number of days you would like the forecast? (1 - 10) ")
 
     if int(number_of_days) <= 10:
         forecast(city,int(number_of_days))
